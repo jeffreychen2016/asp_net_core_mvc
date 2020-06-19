552c242af0199036dd332c5759238943269476de
Jeffrey Chen <jeffreychen@BNAJCHENMAC.local>
2020-06-18 19:52:11 -0500

    feat: add EditRole functionality

--- Controllers/AdministrationController.cs
+++ Controllers/AdministrationController.cs
@@ -10,10 +10,12 @@ namespace EmployeeManagement.Controllers
     public class AdministrationController : Controller
     {
         private readonly RoleManager<IdentityRole> roleManager;
+        private readonly UserManager<ApplicationUser> userManager;
 
-        public AdministrationController(RoleManager<IdentityRole> roleManager)
+        public AdministrationController(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)
         {
             this.roleManager = roleManager;
+            this.userManager = userManager;
         }
 
         [Route("[action]")]
@@ -60,6 +62,70 @@ namespace EmployeeManagement.Controllers
         {
             var roles = roleManager.Roles;
             return View(roles);
+        }
+
+        [Route("[action]")]
+        // Role ID is passed from the URL to the action
+        [HttpGet]
+        public async Task<IActionResult> EditRole(string id)
+        {
+            // Find the role by Role ID
+            var role = await roleManager.FindByIdAsync(id);
+
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with Id = {id} cannot be found";
+                return View("NotFound");
+            }
+
+            var model = new EditRoleViewModel
+            {
+                Id = role.Id,
+                RoleName = role.Name
+            };
+
+            // Retrieve all the Users
+            var users = await userManager.GetUsersInRoleAsync(role.Name);
+            foreach (var user in users)
+            {
+                model.Users.Add(user.UserName);
+            }
+
+            return View(model);
+        }
+
+
+        [Route("[action]")]
+        // This action responds to HttpPost and receives EditRoleViewModel
+        [HttpPost]
+        public async Task<IActionResult> EditRole(EditRoleViewModel model)
+        {
+            var role = await roleManager.FindByIdAsync(model.Id);
+
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with Id = {model.Id} cannot be found";
+                return View("NotFound");
+            }
+            else
+            {
+                role.Name = model.RoleName;
+
+                // Update the Role using UpdateAsync
+                var result = await roleManager.UpdateAsync(role);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("ListRoles");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+                return View(model);
+            }
         }
     }
 }
